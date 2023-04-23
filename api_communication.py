6a59b865e754603ddbdf3e042cabe56f7886d782
Soumya Gautam <101983954+soumyagautam@users.noreply.github.com>
2023-04-23 11:25:45 +0530

    Minor Help Change

--- api_communication.py
+++ api_communication.py
@@ -9,6 +9,7 @@ headers = {
 }
 
 
+# Uploading the audio file to the Assembly AI Server
 def upload(filename):
     def read_file(start_filename, chunk_size=5242880):
         with open(start_filename, 'rb') as _file:
@@ -27,6 +28,7 @@ def upload(filename):
     return audio_url
 
 
+# Posting the request for transcription
 def transcribe(audio_url, sentiment_analysis):
     transcript_request = {
         "audio_url": audio_url,
@@ -37,12 +39,14 @@ def transcribe(audio_url, sentiment_analysis):
     return job_id
 
 
+# Checking whether the transcription is ready or not
 def poll(transcript_id):
     polling_endpoint = transcript_endpoint + "/" + transcript_id
     polling_response = requests.get(polling_endpoint, headers=headers)
     return polling_response.json()
 
 
+# Returns the polling response after polling is completed and check whether there was an error in this process
 def get_transcription_result_url(audio_url, sentiment_analysis):
     transcript_id = transcribe(audio_url, sentiment_analysis)
     while True:
@@ -55,6 +59,7 @@ def get_transcription_result_url(audio_url, sentiment_analysis):
         time.sleep(30)
 
 
+# Saving transcription and sentiment analysis or error
 def save_transcript(audio_url, title, sentiment_analysis=False):
     data, error = get_transcription_result_url(audio_url, sentiment_analysis)
 
